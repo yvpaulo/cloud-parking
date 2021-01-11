a305fd62a2aec59c3276012e3e64373fdaa9f1df
yvpaulo@yahoo.com.br <yvpaulo@yahoo.com.br>
2021-01-11 13:54:13 -0300

    estrutura inicial

---
+++ src/main/java/com/yvsistemas/cloudparking/CloudParkingApplication.java
@@ -0,0 +1,13 @@
+package com.yvsistemas.cloudparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkingApplication.class, args);
+	}
+
+}
